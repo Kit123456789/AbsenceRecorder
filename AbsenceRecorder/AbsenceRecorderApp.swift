c1c7947f2a1a0ce53ecbbc07d46b7a480c8f9754
Kit Ropner <ropner.k@etoncollege.org.uk>
2021-03-02 08:34:05 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Kit Ropner on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
